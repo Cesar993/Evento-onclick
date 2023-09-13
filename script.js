d92d353243160681a029741d6aef28f23d2bae40
Cesar993 <ce.serrano@duocuc.cl.com>
2023-09-13 16:13:58 -0300

    On click

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function changeLogin(element) {
+    if (element.innerText =="Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+        
+    }
+}
+function remove(element) {
+    element.remove();
+}
+function like(params) {
+    alert("Ninja was liked")
+}
+function likeButton() {
+    alert("Ninja was liked")
+}
+
